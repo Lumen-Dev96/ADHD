6bafa0d9aaaea20a58bf29413bdb41f49e35fab2
Lumen <lumen@slingshot.sh>
2024-01-24 09:25:27 +0800

    Refactor v1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ADHD
+
+This project is for ESP8266 only
+
+1 channel 14 classes recognization
--- modeling/2-2classes/adhd_model/Saved_Model_convertor.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-
-# Convert the model
-converter = tf.lite.TFLiteConverter.from_saved_model('/home/mqiu/hongmin_new/two/result/1220/1904/test100_1.pb') # path to the SavedModel directory
-tflite_model = converter.convert()
-
-# Save the model.
-with open('/home/mqiu/hongmin_new/two/result/1220/1904/test100_1_S.tflite', 'wb') as f:
-  f.write(tflite_model)
