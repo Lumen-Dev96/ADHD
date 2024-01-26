7315ae0efd0a1399342666fe9ba2724f5645ef8a
Lumen <lumen@slingshot.sh>
2024-01-26 16:28:45 +0800

    Add real-time matplotlib

--- src/client/monitor.py
+++ src/client/monitor.py
@@ -18,155 +18,6 @@ from math import *
 
 
 
-# 初始化數據/設定
-data_length = 30
-
-data_channels = 12
-
-x = np.arange(data_length)
-
-y_data = [np.zeros(data_length) for _ in range(data_channels + 1)]
-
-grap_id = 0
-
-record_count = 0
-
-rawData = []
-
-topic = "AD1"
-
-max_data_count = 10000
-
-stop_time_seconds = 100
-
-trained_data = pd.read_csv('../../public/data/csv/a1_1.csv')
-trained_data_x = trained_data.iloc[:, 1:-1].values
-
-times_arr = []
-
-########### LOAD MODEL ###############
-# 初始化一个空列表来保存预测结果
-predictions = []
-model_path = '../../public/pb/test30_1.pb'
-x_record = 1
-model = load_model(model_path)
-########### LOAD MODEL ###############
-
-# 初始化 Bokeh 的 ColumnDataSource
-source1 = ColumnDataSource(data={'x': x, 'y': y_data[1]})
-source2 = ColumnDataSource(data={'x': x, 'y': y_data[2]})
-source3 = ColumnDataSource(data={'x': x, 'y': y_data[3]})
-
-source4 = ColumnDataSource(data={'x': x, 'y': y_data[4]})
-source5 = ColumnDataSource(data={'x': x, 'y': y_data[5]})
-source6 = ColumnDataSource(data={'x': x, 'y': y_data[6]})
-
-source7 = ColumnDataSource(data={'x': x, 'y': y_data[7]})
-source8 = ColumnDataSource(data={'x': x, 'y': y_data[8]})
-source9 = ColumnDataSource(data={'x': x, 'y': y_data[9]})
-
-source10 = ColumnDataSource(data={'x': x, 'y': y_data[10]})
-source11 = ColumnDataSource(data={'x': x, 'y': y_data[11]})
-source12 = ColumnDataSource(data={'x': x, 'y': y_data[12]})
-
-source13 = ColumnDataSource(data={'x': np.arange(1), 'y': np.zeros(1)})
-
-
-
-# 創建 Bokeh 的線型圖
-plots = []
-plot1 = figure(height=300, title="MPU1", tools="crosshair,pan,reset,save,wheel_zoom")
-line_ax1 = plot1.line('x', 'y', source=source1, line_width=2, line_alpha=0.8, line_color='black', legend_label='AX1')
-line_ay1 = plot1.line('x', 'y', source=source2, line_width=2, line_alpha=0.8, line_color='blue', legend_label='AY1')
-line_az1 = plot1.line('x', 'y', source=source3, line_width=2, line_alpha=0.8, line_color='red', legend_label='AZ1')
-line_gx1 = plot1.line('x', 'y', source=source4, line_width=2, line_alpha=0.8, line_color='green', legend_label='GX1')
-line_gy1 = plot1.line('x', 'y', source=source5, line_width=2, line_alpha=0.8, line_color='yellow', legend_label='GY1')
-line_gz1 = plot1.line('x', 'y', source=source6, line_width=2, line_alpha=0.8, line_color='pink', legend_label='GZ1')
-
-
-
-# plot2 = figure(plot_height=300, title="Channel AY1", tools="crosshair,pan,reset,save,wheel_zoom")
-# line = plot2.line('x', 'y', source=source2, line_width=2, line_alpha=0.8)
-
-# plot3 = figure(plot_height=300, title="Channel AZ1", tools="crosshair,pan,reset,save,wheel_zoom")
-# line = plot3.line('x', 'y', source=source3, line_width=2, line_alpha=0.8)
-
-# plot4 = figure(plot_height=300, title="Channel GX1", tools="crosshair,pan,reset,save,wheel_zoom")
-# line = plot4.line('x', 'y', source=source4, line_width=2, line_alpha=0.8)
-
-# plot5 = figure(plot_height=300, title="Channel GY1", tools="crosshair,pan,reset,save,wheel_zoom")
-# line = plot5.line('x', 'y', source=source5, line_width=2, line_alpha=0.8)
-
-# plot6 = figure(plot_height=300, title="Channel GZ1", tools="crosshair,pan,reset,save,wheel_zoom")
-# line = plot6.line('x', 'y', source=source6, line_width=2, line_alpha=0.8)
-
-plot7 = figure(height=300, title="MPU2", tools="crosshair,pan,reset,save,wheel_zoom")
-line_ax2 = plot7.line('x', 'y', source=source7, line_width=2, line_alpha=0.8, line_color='black', legend_label='AX2')
-line_ay2 = plot7.line('x', 'y', source=source8, line_width=2, line_alpha=0.8, line_color='blue', legend_label='AY2')
-line_az2 = plot7.line('x', 'y', source=source9, line_width=2, line_alpha=0.8, line_color='red', legend_label='AZ2')
-line_gx2 = plot7.line('x', 'y', source=source10, line_width=2, line_alpha=0.8, line_color='green', legend_label='GX2')
-line_gy2 = plot7.line('x', 'y', source=source11, line_width=2, line_alpha=0.8, line_color='yellow', legend_label='GY2')
-line_gz2 = plot7.line('x', 'y', source=source12, line_width=2, line_alpha=0.8, line_color='pink', legend_label='GZ2')
-
-
-# plot8 = figure(plot_height=300, title="Channel AY2", tools="crosshair,pan,reset,save,wheel_zoom")
-# line = plot8.line('x', 'y', source=source8, line_width=2, line_alpha=0.8)
-
-# plot9 = figure(plot_height=300, title="Channel AZ2", tools="crosshair,pan,reset,save,wheel_zoom")
-# line = plot9.line('x', 'y', source=source9, line_width=2, line_alpha=0.8)
-
-# plot10 = figure(plot_height=300, title="Channel GX2", tools="crosshair,pan,reset,save,wheel_zoom")
-# line = plot10.line('x', 'y', source=source10, line_width=2, line_alpha=0.8)
-
-# plot11 = figure(plot_height=300, title="Channel GY2", tools="crosshair,pan,reset,save,wheel_zoom")
-# line = plot11.line('x', 'y', source=source11, line_width=2, line_alpha=0.8)
-
-# plot12 = figure(plot_height=300, title="Channel GZ2", tools="crosshair,pan,reset,save,wheel_zoom")
-# line = plot12.line('x', 'y', source=source12, line_width=2, line_alpha=0.8)
-
-# plot13 = figure(height=300, title="Predictions", tools="crosshair,pan,reset,save,wheel_zoom")
-# line = plot13.line('x', 'y', source=source13, line_width=2, line_alpha=0.8, line_color='black')
-
-
-plots.append(plot1)
-plots.append(plot7)
-# plots.append(plot13)
-# plots.append(plot3)
-# plots.append(plot4)
-# plots.append(plot5)
-# plots.append(plot6)
-# plots.append(plot7)
-# plots.append(plot8)
-# plots.append(plot9)
-# plots.append(plot10)
-# plots.append(plot11)
-# plots.append(plot12)
-
-
-grid = layout(plots, sizing_mode='scale_width')
-
-
-# 在Jupyter Notebook中顯示
-# output_notebook()
-
-# show(grid, notebook_handle=True)
-
-
-plt.ion() #开启interactive mode 成功的关键函数
-plt.figure(1)
-t = [0]
-t_now = 0
-m = [sin(t_now)]
-
-colors = ["orange", "purple", "green", "red", "blue", "grey"]
-
-plt.gca().set_prop_cycle(color=colors)
-plt.figure(figsize=(12.8, 7.2))
-
-
-
-
-
 
 def transformTimestamps(timestamp):
     # d = datetime.datetime.fromtimestamp(timestamp/1000)
@@ -181,16 +32,15 @@ def update(message):
     global x, y_data, grap_id, x_record, rawData, record_count, max_data_count, stop_time_seconds
     
     # 解碼 MQTT 數據
-    payload = message.payload.decode()
-    data = json.loads(payload)
+    # payload = message.payload.decode()
+    # data = json.loads(payload)
+
+    # test mode
+    data = message
 
     
     mqData = np.array(data)
     # fifoData = np.array(data[1])
-
-    if len(mqData) == 0:
-        print('No data')
-        return
 
 
     times = mqData[:, 0]
@@ -261,24 +111,6 @@ def update(message):
 
     # 更新數據源的數據
     grap_id += data_length
-    source1.stream({'x':np.arange(grap_id, grap_id + data_length), 'y':channel1})
-    source2.stream({'x':np.arange(grap_id, grap_id + data_length), 'y':channel2})
-    source3.stream({'x':np.arange(grap_id, grap_id + data_length), 'y':channel3})
-    source4.stream({'x':np.arange(grap_id, grap_id + data_length), 'y':channel4})
-    source5.stream({'x':np.arange(grap_id, grap_id + data_length), 'y':channel5})
-    source6.stream({'x':np.arange(grap_id, grap_id + data_length), 'y':channel6})
-    source7.stream({'x':np.arange(grap_id, grap_id + data_length), 'y':channel7})
-    source8.stream({'x':np.arange(grap_id, grap_id + data_length), 'y':channel8})
-    source9.stream({'x':np.arange(grap_id, grap_id + data_length), 'y':channel9})
-    source10.stream({'x':np.arange(grap_id, grap_id + data_length), 'y':channel10})
-    source11.stream({'x':np.arange(grap_id, grap_id + data_length), 'y':channel11})
-    source12.stream({'x':np.arange(grap_id, grap_id + data_length), 'y':channel12})
-    
-    #push_notebook(handle=handle)
-
-    push_notebook()
-
-    curdoc().add_next_tick_callback(show_plot)
 
     record_count = record_count + 1
 
@@ -286,19 +118,26 @@ def update(message):
 
 
     # plt.clf() # 清空画布上的所有内容。此处不能调用此函数，不然之前画出的点，将会被清空。
-    t_now = record_count * 0.1
-    plt.plot(times, channel1, label='AX1')
-    plt.plot(times, channel2, label='AY1')
-    plt.plot(times, channel3, label='AZ1')
-
-
-    # plt.scatter(t_now, sin(t_now)) 
-
+    plt.sca(mpu1)    
+    plt.plot(times, channel1, label='AX1', color="orange")
+    plt.plot(times, channel2, label='AY1', color="purple")
+    plt.plot(times, channel3, label='AZ1', color="green")
+    plt.plot(times, channel4, label='GX1', color="red")
+    plt.plot(times, channel5, label='GY1', color="blue")
+    plt.plot(times, channel6, label='GZ1', color="grey")
     plt.pause(0.01)#注意此函数需要调用
 
-   
-def show_plot():
-    show(grid)
+    print(channel1)
+
+
+    plt.sca(mpu2)    
+    plt.plot(times, channel7, label='AX2', color="orange")
+    plt.plot(times, channel8, label='AY2', color="purple")
+    plt.plot(times, channel9, label='AZ2', color="green")
+    plt.plot(times, channel10, label='GX2', color="red")
+    plt.plot(times, channel11, label='GY2', color="blue")
+    plt.plot(times, channel12, label='GZ2', color="grey")
+    plt.pause(0.01)#注意此函数需要调用
 
 
 
@@ -356,7 +195,6 @@ def broadcast(command):
                 client.send(command.encode('utf-8'))  # Send the command to each client
             except socket.error as e:  # Handle potential socket errors
                 print(f'Error sending to client: {e}')
-
 
 def client_thread(client_socket, client_address):
     global data_buffer
@@ -467,39 +305,97 @@ def broadcast_thread():
 
 
 
-def save_recieved_data(data ,file_path = None):
-    IMU_Detected_Data = decode_buffer(data)
-    IMU_Detected_Data.to_csv(file_path, index=False)
-    print('export csv success')
-
-    return
-
-def close_sockets():
-    server_socket.close()
-    with clients_lock:
-        for client in clients:
-            client.close()
-    print('Closed all client connections')
 
 
+# 初始化數據/設定
+data_length = 30
 
-nickname = ""
-# List to keep track of client connections
-clients = []
-# Lock to ensure thread-safe operations on the clients list
-clients_lock = threading.Lock()
-Special_Command = ""
-# Server configuration
-server_ip = '0.0.0.0'  # Listen on all network interfaces
-server_port = 10000
-Recieved_Data_in_Real_Time =[]
-data_buffer =[]
-Buffer_of_IMU2 =[]
-Running_IMU = None
-save_file_path = "../../public/data/csv/validation.csv"
-# Create a TCP/IP socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+data_channels = 12
 
-connect_mqtt_server()
+x = np.arange(data_length)
 
+y_data = [np.zeros(data_length) for _ in range(data_channels + 1)]
+
+grap_id = 0
+
+record_count = 0
+
+rawData = []
+
+topic = "AD1"
+
+max_data_count = 10000
+
+stop_time_seconds = 100
+
+trained_data = pd.read_csv('../../public/data/csv/a1_1.csv')
+trained_data_x = trained_data.iloc[:, 1:-1].values
+
+times_arr = []
+
+########### LOAD MODEL ###############
+# 初始化一个空列表来保存预测结果
+predictions = []
+model_path = '../../public/pb/test30_1.pb'
+x_record = 1
+model = load_model(model_path)
+########### LOAD MODEL ###############
+
+
+
+
+plt.ion() #开启interactive mode 成功的关键函数
+t = [0]
+t_now = 0
+m = [sin(t_now)]
+
+plt.figure(figsize=(18, 10.8), dpi=100)
+
+
+#第一行第一列图形
+mpu1 = plt.subplot(2,1,1)
+#第一行第二列图形
+mpu2 = plt.subplot(2,1,2)
+
+           
+plt.sca(mpu1)       
+plt.title("MPU1", fontsize=15)
+plt.xlabel("Timestamp", fontsize=13)
+plt.ylabel("Sensor", fontsize=13)
+plt.xticks(rotation=90)
+mpu_y_ticks = np.arange(-50, 50, 1)
+plt.yticks(mpu_y_ticks)
+
+plt.sca(mpu2)    
+plt.title("MPU2", fontsize=15)
+plt.xlabel("Timestamp", fontsize=13)
+plt.ylabel("Sensor", fontsize=13)
+plt.xticks(rotation=90)
+mpu_y_ticks = np.arange(-50, 50, 1)
+plt.yticks(mpu_y_ticks)
+
+
+test_mode = True
+# connect_mqtt_server()
+
+while test_mode:
+    test_data = []
+    for i in range(30):
+        test_data.append([
+            time.time(), 
+            1.0123,
+            1.012,
+            1.0123,
+            1.0123,
+            1.0123,
+            1.0123,
+            1.0123,
+            -20.0123,
+            1.0123,
+            1.0123,
+            10.0123,
+            1.0123,
+        ])
+    update(test_data)
+    time.sleep(0.5)
 
